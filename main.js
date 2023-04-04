ec9ebc2a9c44fbde2ccd411eb1f17567860461ae
Thomas <thomas8483828180@gmail.com>
2023-04-04 16:19:23 -0500

    rotating Sun thing?

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
 const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(1200, -250, 20000);
 // FoV, aspect ratio (width/height), cutoff values
 
 const renderer = new THREE.WebGLRenderer();
@@ -15,7 +14,7 @@ document.body.appendChild(renderer.domElement);
 
 // Skybox
 const skyboxGeom = new THREE.BoxGeometry( 10000, 10000, 10000 );
-const skyboxMaterial = new THREE.MeshBasicMaterial( { color:0x000000 } );
+const skyboxMaterial = new THREE.MeshBasicMaterial( { color:0xffffff } );
 const skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
 
 // Main Sun
@@ -23,26 +22,31 @@ const sunGeom = new THREE.SphereGeometry( 5, 10, 10 );
 const sunMaterial = new THREE.MeshBasicMaterial( { color:0x111111 } );
 const sun = new THREE.Mesh(sunGeom, sunMaterial);
 
+// 2 ways to create a line
+const sunEdges = new THREE.EdgesGeometry(sunGeom);
+const sunOutlineMaterial = new THREE.LineBasicMaterial({color: 0xfff700});
+const sunOutline = new THREE.LineSegments(sunEdges, sunOutlineMaterial);
 
+// do all animation in this function
 function animate() {
     requestAnimationFrame(animate);
-    // do all animation here
 
     //skybox
     skybox.rotation.x += 0.005;
     skybox.rotation.y += 0.005;
 
+    sun.rotation.y += 0.005;
 
-    sun.rotation.x += 0.01;
-    sun.rotation.y += 0.01;
+    sunOutline.rotation.y += 0.005;
 
     renderer.render(scene, camera);
 }
 
-animate();
-
 // add wanted objects to scene
 scene.add(sun);
+scene.add(skybox);
+scene.add(sunOutline);
 
+animate();
 
 camera.position.set(0,0,10);
