dccfa2f5ae63c953f3e2ff8f210a31e1f491dace
Thomas <thomas8483828180@gmail.com>
2023-04-05 14:16:20 -0500

    Func

--- main.js
+++ main.js
@@ -27,69 +27,87 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 
-// create any sphere
-// name: the name of the object being projected, in lower case (i.e. mercury)
-// radius: the radius of the sphere (i.e. 1.0)
-function createSphere(name, radius, x, ringParams) {
-	
-	// inject name into a path string
-	const path = "assets/maps/" + name + ".png";
+// This function creates a celestial body
+// PARAMETERS;
+// bodyName: The name of the body as a lowercase String (for example: "mercury")
+// bodyRadius: The radius of the body (for example: 20.0)
+// 
 
-	// create the sphere
-	const geom = new THREE.SphereGeometry(radius);
-	const texture = new THREE.TextureLoader().load(path);
-	const material = new THREE.MeshStandardMaterial({
-		map: texture,
+function createBody(bodyName, bodyRadius, distance, ringRadii) {
+
+	// Create the body's geometry using the body's Radius
+	const bodyGeom = new THREE.SphereGeometry(bodyRadius);
+
+	// Create a path name for the body texture image file, then use that to make a body texture
+	const bodyPath = "assets/maps/" + bodyName + ".png";
+	const bodyTexture = new THREE.TextureLoader().load(bodyPath);
+
+	// Use the body texture and body material to make a body mesh
+	const bodyMat = new THREE.MeshStandardMaterial({
+		map: bodyTexture,
 	});
 	
-	// create planet
-	const planet = new THREE.Mesh(geom, material);
-	const planetObj = new THREE.Object3D();
-	planetObj.add(planet);
+	// Create a planet
+	const body = new THREE.Mesh(bodyGeom, bodyMat);
 
-	// if there is a ring, create one
-	if (ringParams) {
+	// Create a pivot to control the planet's orbit around the Sun, then add the body to the pivot
+	const pivot = new THREE.Object3D();
+	pivot.add(body);
+
+	// Add the pivot and set the body's distance from the Sun
+	scene.add(pivot);
+	body.position.set(distance, 0, 0);
+
+	// This if statement is run if the ring's inner and outer radii are passed in a list
+	if (ringRadii) {
+		
 		const ringGeom = new THREE.RingGeometry(
-			ringParams.innerRadius, 
-			ringParams.outerRadius, 
-			32
+			ringRadii.innerRadius, 
+			ringRadii.outerRadius
 		);
-		const ringPath = "assets/maps/" + name + "Ring.png";
+		
+		// Make a path name for the ring texture image file, then use that to make a ring texture
+		const ringPath = "assets/maps/" + bodyName + "Ring.png";
 		const ringTexture = new THREE.TextureLoader().load(ringPath);
-		const ringMaterial = new THREE.MeshBasicMaterial({
+
+		// Use the ring geometry and ring material to make a ring mesh
+		const ringMat = new THREE.MeshBasicMaterial({
 			map: ringTexture,
 			side: THREE.DoubleSide
 		});
-		const ring = new THREE.Mesh(ringGeom, ringMaterial);
-		planetObj.add(ring);
-		ring.position.set(x, 0, 0);
+		const ring = new THREE.Mesh(ringGeom, ringMat);
+
+		// Add the ring to the pivot and set its distance from the Sun
+		pivot.add(ring);
+		ring.position.set(distance, 0, 0);
 		ring.rotation.x = -0.5 * Math.PI;
+
+		// Return body, ring, pivot so they can be accessed later
+		return {body, ring, pivot}
 
 	}
 
-	scene.add(planetObj);
-	planet.position.set(x, 0, 0);
-
-	return {planet, planetObj}
+	// If ring is not rendered, just return a body and pivot
+	return {body, pivot}
 }
 
 // Sun
 const sunGeom = new THREE.SphereGeometry(5);
 const sunMaterial = new THREE.MeshBasicMaterial( { color:0xffffff } );
 const sun = new THREE.Mesh(sunGeom, sunMaterial);
-const pointLight = new THREE.PointLight(0xffffff, 2, 500);
+const pointLight = new THREE.PointLight(0xffffff, 1.3, 0);
 
 // Main planets
-const mercury = createSphere("mercury", 1, 25);
-const venus = createSphere("venus", 3, 50);
-const earth = createSphere("earth", 3, 75);
-const mars = createSphere("mars", 1.5, 100);
-const jupiter = createSphere("jupiter", 10, 200);
+const mercury = createBody("mercury", 1, 25);
+const venus = createBody("venus", 3, 50);
+const earth = createBody("earth", 3, 75);
+const mars = createBody("mars", 1.5, 100);
+const jupiter = createBody("jupiter", 10, 200);
 
-const saturn = createSphere("saturn", 9, 300, {innerRadius: 10, outerRadius: 20});
+const saturn = createBody("saturn", 9, 300, {innerRadius: 10, outerRadius: 20});
 
-const uranus = createSphere("uranus", 6, 400);
-const neptune = createSphere("neptune", 6, 500);
+const uranus = createBody("uranus", 6, 400);
+const neptune = createBody("neptune", 6, 500);
 
 // add wanted objects to scene
 scene.add(sun);
@@ -106,16 +124,16 @@ function animate() {
 	// planetObj.rotation.y += YEAR LENGTH
 
 	// Mercury
-	mercury.planet.rotation.y += 0.0001;
-	mercury.planetObj.rotation.y += 0.008;
+	mercury.body.rotation.y += 0.0001;
+	mercury.pivot.rotation.y += 0.008;
 
 	// Venus
-	venus.planet.rotation.y += 0.0001;
-	venus.planetObj.rotation.y += 0.006;
+	venus.body.rotation.y += 0.0001;
+	venus.pivot.rotation.y += 0.006;
 
 	// Earth
-	earth.planet.rotation.y += 0.0001;
-	earth.planetObj.rotation.y += 0.006;
+	earth.body.rotation.y += 0.0001;
+	earth.pivot.rotation.y += 0.006;
 
 
     renderer.render(scene, camera);
