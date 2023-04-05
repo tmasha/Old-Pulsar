df08cb3a93f5a950158349ea8e474ff37b3d9692
Thomas <thomas8483828180@gmail.com>
2023-04-04 19:02:38 -0500

    Revert BAD code.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import * as THREE from 'three';
 
-// create scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
-// FoV, aspect ratio (width / height), cutoff values
-const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 45, 30000 );
-camera.position.set(1200, -250, 2000);
+const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0,0,10);
+// FoV, aspect ratio (width/height), cutoff values
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -14,35 +13,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-function createPathStrings(filename) {
-    const basePath = "./src/assets/skybox"
-    const baseFilename = basePath + filename;
-    const fileType = ".jpg";
-    const sides = ["front", "back", "up", "down", "right", "left"];
-
-    const pathStrings = sides.map(side => {
-        return baseFilename + "_" + side + fileType;
-    })
-    
-    return pathStrings;
-}
-
-function createMaterialArray(filename) {
-    const skyboxImagePaths = createPathStrings(filename);
-    const materialArray = skyboxImagePaths.map(image => {
-        let texture = new THREE.TextureLoader().load(image);
-        return texture;
-      });
-
-      return materialArray;
-}
-
-const skyboxImage = 'sky';
-
 // Skybox
 const skyboxGeom = new THREE.BoxGeometry( 10000, 10000, 10000 );
-const skyboxMaterial = createMaterialArray(skyboxImage);
-
+const skyboxMaterial = new THREE.MeshBasicMaterial( { color:0xffffff } );
 const skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
 
 // Main Sun
